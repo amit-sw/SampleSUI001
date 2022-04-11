a749af9ff442928ec695bf0de277ca4130c6fcd9
Amit Gupta <streamlit@gprof.com>
2022-04-11 11:32:12 -0700

    Initial Commit

---
+++ SampleSUI001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleSUI001
+//
+//  Created by Amit Gupta on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleSUI001/SampleSUI001App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSUI001App.swift
+//  SampleSUI001
+//
+//  Created by Amit Gupta on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleSUI001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
